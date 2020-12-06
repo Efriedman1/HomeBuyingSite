fd3c536127ff5ec48990b69e276cb3e1e0283ac6
jnovak96 <47129558+jnovak96@users.noreply.github.com>
2020-12-06 00:17:27 -0500

    barebones ascx control for property

---
+++ 3342FinalProject/PropertyControl.ascx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.ComponentModel;
+using System.Web.UI.WebControls;
+
+namespace _3342FinalProject
+{
+    public partial class PropertyControl : System.Web.UI.UserControl
+    {
+        String imageUrl;
+        String propertyName;
+        String propertyAddress;
+        String propertyDescription;
+        int bedAmount;
+        int bathAmount;
+        double rentAmount;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [Category("Misc")]
+        public String ImageUrl
+        {
+            get { return imageUrl; }
+            set { imageUrl = value; }
+        }
+
+        [Category("Misc")]
+        public String Name
+        {
+            get { return propertyName; }
+            set { propertyName = value; }
+        }
+
+        [Category("Misc")]
+        public String Address
+        {
+            get { return propertyAddress; }
+            set { propertyAddress = value; }
+        }
+
+        [Category("Misc")]
+        public String Description
+        {
+            get { return propertyDescription; }
+            set { propertyDescription = value; }
+        }
+
+        [Category("Misc")]
+        public int Beds
+        {
+            get { return bedAmount; }
+            set { bedAmount = value; }
+        }
+
+        [Category("Misc")]
+        public int Baths
+        {
+            get { return bathAmount; }
+            set { bathAmount = value; }
+        }
+    }
+}
--- PropertyLibrary/Utility.cs
+++ PropertyLibrary/Utility.cs
@@ -19,5 +19,7 @@ namespace PropertyLibrary
         {
             return true;
         }
+
+
     }
 }
